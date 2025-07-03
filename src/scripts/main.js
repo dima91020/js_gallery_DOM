2ad5cd9c1f83ddf9fa0e464035aece20c26c61ad
dima91020 <vereskodima76@gmail.com>
2025-07-03 23:51:05 +0300

    add main.js file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const galeryList = document.querySelector('.gallery__list');
+const mainImage = document.querySelector('.gallery__large-img');
+
+// eslint-disable-next-line
+galeryList.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const listItemLink = event.target.closest('.list-item__link');
+
+  if (!listItemLink) {
+    return;
+  }
+
+  mainImage.src = listItemLink.href;
+});
